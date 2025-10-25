a0035ba29ad61eaa575bd684efd3d20aeed34f2d
Jordan Scales <scalesjordan@gmail.com>
2025-10-25 08:51:58 -0400

    add glyph stacking

--- lib/ansi_shadow/glyph.rb
+++ lib/ansi_shadow/glyph.rb
@@ -5,13 +5,55 @@ module AnsiShadow
   # Represents an individual character or "glyph" of text
   class Glyph
     ##
-    # @param [Array[String]]
+    # @param [Array<String>] grid
     def initialize(grid)
       @grid = grid
     end
 
     def to_s
       @grid.map(&:join).join "\n"
+    end
+
+    ##
+    # Returns the pixel at (px_x, px_y), or nil if it
+    # is out of bounds
+    #
+    # @param [number] px_x
+    # @param [number] px_y
+    def pixel(px_x, px_y)
+      @grid[px_y] && @grid[px_y][px_x]
+    end
+
+    ##
+    # Returns the width of the glyph, which is the
+    # max length of all rows
+    def width
+      @grid.map(&:size).max
+    end
+
+    ##
+    # Returns the height of the glyph
+    def height
+      @grid.size
+    end
+
+    ##
+    # Stacks this glyph on top of another, and returns
+    # the result as a new Glyph
+    #
+    # @param [Glyph] The other glyph
+    # @return [Glyph]
+    def atop(other_glyph)
+      grid = []
+      [height, other_glyph.height].max.times do |y|
+        row = []
+        [width, other_glyph.width].max.times do |x|
+          row << stack_pixel(pixel(x, y), other_glyph.pixel(x, y))
+        end
+        grid << row
+      end
+
+      Glyph.new grid
     end
 
     class << self
@@ -25,6 +67,21 @@ module AnsiShadow
         end
 
         Glyph.new grid
+      end
+    end
+
+    private
+
+    ##
+    # "Stacks" a pixel on top of another
+    #
+    # @param [String, nil] px1
+    # @param [String, nil] px2
+    def stack_pixel(px1, px2)
+      if px1.nil? || /\s/ =~ px1
+        px2
+      else
+        px1
       end
     end
   end
--- spec/ansi_shadow_spec.rb
+++ spec/ansi_shadow_spec.rb
@@ -14,4 +14,25 @@ RSpec.describe AnsiShadow::Glyph do
       ███
     TEXT
   end
+
+  it "can stack glyphs" do
+    glider = AnsiShadow::Glyph.from_mono <<~TEXT
+       x
+        x
+      xxx
+    TEXT
+
+    top_right_box = AnsiShadow::Glyph.new(
+      [
+        " **".chars,
+        " **".chars
+      ]
+    )
+
+    expect(glider.atop(top_right_box).to_s).to eq <<~TEXT.chomp
+       █*
+       *█
+      ███
+    TEXT
+  end
 end
