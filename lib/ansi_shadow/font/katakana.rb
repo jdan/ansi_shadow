3d7ac8759108f2744b687797e23848ac79a3b355
Jordan Scales <scalesjordan@gmail.com>
2025-10-25 17:08:07 -0400

    add AnsiShadow::Text

--- lib/ansi_shadow/font/katakana.rb
+++ lib/ansi_shadow/font/katakana.rb
@@ -466,6 +466,32 @@ module AnsiShadow
           ██████
         ████
       TEXT
+
+      DASH = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+
+
+
+        ████████████
+
+
+
+
+      TEXT
+
+      KANA_MAP = {
+        ア: A, イ: I, ウ: U, エ: E, オ: O,
+        カ: KA, キ: KI, ク: KU, ケ: KE, コ: KO,
+        サ: SA, シ: SHI, ス: SU, セ: SE, ソ: SO,
+        タ: TA, チ: CHI, ツ: TSU, テ: TE, ト: TO,
+        ナ: NA, ニ: NI, ヌ: NU, ネ: NE, ノ: NO,
+        ハ: HA, ヒ: HI, フ: HU, ヘ: HE, ホ: HO,
+        マ: MA, ミ: MI, ム: MU, メ: ME, モ: MO,
+        ヤ: YA, ユ: YU, ヨ: YO,
+        ラ: RA, リ: RI, ル: RU, レ: RE, ロ: RO,
+        ワ: WA, ヲ: WO,
+        ン: N,
+        ー: DASH
+      }.freeze
     end
   end
   # rubocop:enable Metrics/ModuleLength
---
+++ lib/ansi_shadow/text.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module AnsiShadow
+  ##
+  # Renders text from an input string
+  class Text
+    def initialize(glyphs)
+      @glyphs = glyphs
+    end
+
+    def to_s
+      @glyphs.inject(&:beside).to_s
+    end
+
+    def shadow
+      Text.new(@glyphs.map(&:shadow))
+    end
+
+    class << self
+      def from_str(str)
+        glyphs = str.chars.map { |char| Font::Katakana::KANA_MAP[char.to_sym] }
+        Text.new glyphs
+      end
+    end
+  end
+end
