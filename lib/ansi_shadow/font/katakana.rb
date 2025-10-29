ea7ec177fbb3a36944ae37f1012d02e3b4545c88
Jordan Scales <scalesjordan@gmail.com>
2025-10-29 12:51:26 -0400

    update sprites from Legend of Zelda (NES)

--- lib/ansi_shadow/font/katakana.rb
+++ lib/ansi_shadow/font/katakana.rb
@@ -3,189 +3,190 @@
 module AnsiShadow
   module Font
     ##
-    # 8x7 Katakana gylphs for ANSI Shadow font
+    # 7x7 Katakana gylphs for ANSI Shadow font
     #
-    # Inspired by the sprites found in Pokémon Blue (JP)
+    # Inspired by the sprites found in The Legend of Zelda (NES)
+    # @see https://legendsoflocalization.com/comparisons/the-legend-of-zelda/gameplay/
     # rubocop:disable Metrics/ModuleLength
     module Katakana
       A = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ████████████████
-            ████    ████
-            ████    ████
-            ████  ████
-            ████
-            ████
-          ████
+        ████████████
+                  ██
+            ██    ██
+            ██████
+            ██
+            ██
+        ████
       TEXT
 
       I = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-                    ████
-                  ████
-                ████
-            ██████
-        ██████  ██
+                  ██
                 ██
-                ██
+              ██
+            ████
+        ████  ██
+              ██
+              ██
       TEXT
 
       U = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-              ████
-        ████████████████
-        ████        ████
-                    ████
-                  ████
-                  ████
-              ██████
+              ██
+        ██████████████
+        ██          ██
+        ██          ██
+                    ██
+                  ██
+            ██████
       TEXT
 
       E = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-          ████████████
-          ████████████
-              ████
-              ████
-              ████
-        ████████████████
-        ████████████████
+
+        ██████████████
+              ██
+              ██
+              ██
+              ██
+        ██████████████
       TEXT
 
       O = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-                  ██
-        ████████████████
-              ██████
-            ████  ██
-          ████    ██
-        ████      ██
-                ████
+                ██
+        ██████████████
+              ████
+            ██  ██
+          ██    ██
+        ██      ██
+              ████
       TEXT
 
       KA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-            ████
-        ████████████████
-            ████    ████
-            ████    ████
-          ████      ████
-          ████      ████
-        ████      ████
+          ██
+        ████████████
+          ██      ██
+          ██      ██
+          ██      ██
+          ██      ██
+        ██      ██
       TEXT
 
       KI = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-            ████
+              ██
         ████████████
+              ██
         ████████████
-            ████
-        ████████████
-        ████████████
-            ████
+              ██
+              ██
+              ██
       TEXT
 
       KU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-            ██
-            ████████████
-          ████      ████
-        ████        ████
-                    ████
-                  ████
-                ████
+          ██████████
+          ██      ██
+        ██        ██
+                  ██
+                  ██
+                ██
+            ████
       TEXT
 
       KE = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+        ██
+        ██████████████
+        ██      ██
+        ██      ██
+                ██
+                ██
             ████
-            ████████████
-          ████     ███
-        ████       ███
-                   ███
-                ████
-              ████
       TEXT
 
       KO = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
         ████████████
-                ████
-                ████
-                ████
-                ████
-                ████
+                  ██
+                  ██
+                  ██
+                  ██
+                  ██
         ████████████
       TEXT
 
       SA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-          ████    ████
-        ████████████████
-          ████    ████
-          ████    ████
-                  ████
-                ████
-              ████
+          ██      ██
+        ██████████████
+          ██      ██
+          ██      ██
+                  ██
+                  ██
+            ██████
       TEXT
 
       SHI = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ██████
-                  ████
-        ██████    ████
-                  ████
-                  ████
-                ████
-          ████████
+        ████
+                  ██
+        ████      ██
+                  ██
+                ██
+                ██
+        ████████
       TEXT
 
       SU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ████████████████
-                    ████
-                  ████
-                ████
-              ██████
-            ████  ████
-        ██████      ████
+        ████████████
+                  ██
+                ██
+              ██
+            ██  ██
+          ██      ██
+        ██          ██
       TEXT
 
       SE = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
           ██
         ██████████████
-          ██      ████
-          ██    ████
+          ██        ██
+          ██      ██
           ██
-          ████
-            ██████████
+          ██
+          ████████████
       TEXT
 
       SO = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
         ██          ██
-        ████      ████
-          ████    ████
-                ████
-              ████
+        ██          ██
+          ██        ██
+                    ██
+                  ██
+                ██
           ██████
-        ████
       TEXT
 
       TA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-            ████
-          ██████████████
-        ████         ███
-              ████   ███
-                ████ ███
-                  ████
-              ██████
+          ██████████
+          ██      ██
+          ██████████
+        ██        ██
+                  ██
+                  ██
+            ██████
       TEXT
 
       CHI = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-                  ████
-          ██████████
-              ████
-        ████████████████
-              ████
-              ████
-          ██████
+              ██████
+        ████████
+              ██
+        ██████████████
+              ██
+              ██
+            ████
       TEXT
 
       TSU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ██  ██    ████
-        ██  ██    ████
-        ██  ██    ████
-                  ████
-                  ████
-                ████
-            ██████
+        ██  ██    ██
+        ██  ██    ██
+        ██  ██    ██
+                  ██
+                  ██
+                ██
+          ██████
       TEXT
 
       TE = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
@@ -194,106 +195,107 @@ module AnsiShadow
         ██████████████
               ██
               ██
-            ████
+              ██
           ████
       TEXT
 
       TO = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
         ██
         ██
-        ██████
-        ██  ████
-        ██    ██
+        ██
+        ████████
+        ██      ██
         ██
         ██
       TEXT
 
       NA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-                ████
-        ████████████████
-                ████
-                ████
-                ████
-              ████
-            ████
+                ██
+                ██
+        ██████████████
+                ██
+                ██
+                ██
+          ██████
       TEXT
 
       NI = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-
-          ██████████
-
+          ████████
 
 
-        ██████████████
+
+
+
+        ████████████
       TEXT
 
       NU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ██████████████
-                    ██
-            ██    ████
-            ██  ████
-              ████
-            ████  ██
-          ████    ██
+        ████████████
+                  ██
+                  ██
+          ████  ██
+              ██
+            ██  ██
+        ████      ████
       TEXT
 
       NE = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ████████████
+              ██
+        ██████████████
                   ██
-                ████
-              ████
-          ██████████
-        ████  ██  ████
-        ██    ██    ██
+                ██
+            ████████
+        ████  ██    ██
+              ██
       TEXT
 
       NO = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-                  ██
-                  ██
-                ████
                 ██
-              ████
-            ████
-        ██████
+                ██
+                ██
+              ██
+              ██
+            ██
+        ████
       TEXT
 
       HA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-            ██  ██
-            ██  ██
-          ████  ████
-          ██      ██
-        ████      ████
-        ██          ██
-        ██          ██
+              ██
+                ██
+        ██      ██
+        ██        ██
+        ██        ██
+        ██        ██
+        ██        ██
       TEXT
 
       HI = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
         ██
-        ██    ██████
-        ████████
+        ██
+        ████████████
         ██
         ██
-        ████
+        ██
           ██████████
       TEXT
 
       FU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ██████████████
-                  ████
-                  ████
-                ████
-                ████
-              ████
+        ████████████
+                  ██
+                  ██
+                  ██
+                  ██
+                ██
           ██████
       TEXT
 
       HE = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
 
-            ████
-          ████████
-          ██    ████
-        ████      ████
-        ██          ████
+            ██
+          ██  ██
+        ██      ██
+        ██        ██
+                    ██
 
       TEXT
 
@@ -301,56 +303,56 @@ module AnsiShadow
               ██
         ██████████████
               ██
-          ██  ██  ██
-        ████  ██  ████
+              ██
         ██    ██    ██
-            ████
+        ██    ██    ██
+        ██    ██    ██
       TEXT
 
       MA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ████████████████
-                    ████
-          ██      ████
-          ████  ████
-            ██████
-              ████
-                ████
+        ████████████
+                  ██
+                  ██
+                ██
+          ██  ██
+            ██
+              ██
       TEXT
 
       MI = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
         ████████
-            ████████
+                ██████
+          ████
+              ██████
 
-          ████████
-
-        ████████
-            ████████
+        ██████
+              ████████
       TEXT
 
       MU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+                ██
               ██
-            ████
             ██
-          ████    ██
-          ██      ██
-          ██  ████████
-        ████████    ██
+          ██
+        ██        ██
+        ██          ██
+          ████████████
       TEXT
 
       ME = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-
-                    ██
-            ██      ██
-              ██  ██
-                ██
-            ████████
-        ██████      ██
+                  ██
+                  ██
+          ██    ██
+            ██  ██
+              ██
+            ██  ██
+        ████      ██
       TEXT
 
       MO = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-          ██████████
+        ████████████
             ██
-        ██████████████
+        ████████████
             ██
             ██
             ██
@@ -359,72 +361,72 @@ module AnsiShadow
 
       YA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
             ██
-        ██████████████
-            ██    ████
-            ██  ████
-            ██
+        ████████████
+            ██    ██
+            ██    ██
+            ██  ██
             ██
             ██
       TEXT
 
       YU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-
           ████████
                 ██
                 ██
                 ██
                 ██
-        ████████████
+                ██
+        ██████████████
       TEXT
 
       YO = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
         ████████████
                   ██
                   ██
-          ██████████
+        ████████████
                   ██
                   ██
         ████████████
       TEXT
 
       RA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-          ████████████
+        ████████████
 
-        ████████████████
-                      ██
-                    ████
-                  ████
-              ██████
-      TEXT
-
-      RI = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ██        ██
-        ██        ██
-        ██        ██
-        ██        ██
-                ████
-              ████
+        ████████████
+                  ██
+                  ██
+                ██
           ██████
       TEXT
 
+      RI = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+        ██      ██
+        ██      ██
+        ██      ██
+        ██      ██
+                ██
+              ██
+          ████
+      TEXT
+
       RU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-            ██  ██
-            ██  ██
-            ██  ██
-            ██  ██
-            ██  ██    ██
-            ██  ██  ████
-        ████    ██████
+              ██
+          ██  ██
+          ██  ██
+          ██  ██
+          ██  ██    ██
+          ██  ██    ██
+        ██    ██████
       TEXT
 
       RE = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
         ██
         ██
-        ██          ██
-        ██        ████
-        ██      ████
-        ██  ██████
-        ██████
+        ██
+        ██        ██
+        ██        ██
+        ██      ██
+        ████████
       TEXT
 
       RO = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
@@ -438,54 +440,83 @@ module AnsiShadow
       TEXT
 
       WA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ████████████████
-        ██          ████
-        ██          ████
-                  ████
-                  ████
-                ████
+        ████████████
+        ██        ██
+        ██        ██
+                  ██
+                  ██
+                  ██
             ██████
       TEXT
 
       WO = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ████████████████
-                    ████
-                    ████
-          ████████████
-                  ████
-                ████
-            ██████
+        ████████████
+                  ██
+        ████████████
+                  ██
+                  ██
+                ██
+          ██████
       TEXT
 
       N = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-                    ██
-        ███       ████
-           ███    ████
-                ████
-              ████
-          ██████
         ████
+
+                  ██
+                  ██
+                ██
+                ██
+        ████████
       TEXT
 
-      DASH = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+      XA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
 
 
-
-        ████████████
-
-
-
-
-      TEXT
-
-      XTSU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-
-
-
-        ██  ██  ██
-        ██  ██  ██
+        ██████████
                 ██
             ████
+            ██
+            ██
+      TEXT
+
+      XI = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+
+
+                ██
+            ████
+        ██████
+            ██
+            ██
+      TEXT
+
+      XU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+
+
+            ██
+        ██████████
+        ██      ██
+                ██
+            ████
+      TEXT
+
+      XE = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+
+
+          ██████
+            ██
+            ██
+            ██
+        ██████████
+      TEXT
+
+      XO = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+
+
+              ██
+        ██████████
+            ████
+          ██  ██
+        ██    ██
       TEXT
 
       XYA = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
@@ -498,33 +529,13 @@ module AnsiShadow
           ██
       TEXT
 
-      XI = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-
-
-              ██
-            ██
-        ██████
-            ██
-            ██
-      TEXT
-
       XYU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-
 
 
           ██████
               ██
               ██
-        ██████████
-      TEXT
-
-      XE = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-
-
-
-         ████████
-            ██
-            ██
+              ██
         ██████████
       TEXT
 
@@ -538,16 +549,35 @@ module AnsiShadow
         ████████
       TEXT
 
+      XTSU = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+
+
+        ██  ██  ██
+        ██  ██  ██
+                ██
+                ██
+          ██████
+      TEXT
+
       Dakuten = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-          █
-        █  █
-         █
+        ██  ██
       TEXT
 
       Handakuten = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
-        ███
-        █ █
-        ███
+        ██████
+        ██  ██
+        ██████
+      TEXT
+
+      DASH = AnsiShadow::Glyph.from_mono <<~TEXT.chomp
+
+
+
+        ███████████
+
+
+
+
       TEXT
 
       GA = KA.beside(Dakuten)
@@ -597,8 +627,9 @@ module AnsiShadow
         ラ: RA, リ: RI, ル: RU, レ: RE, ロ: RO,
         ワ: WA, ヲ: WO,
         ン: N,
-        ー: DASH, ッ: XTSU,
-        ャ: XYA, ィ: XI, ュ: XYU, ェ: XE, ョ: XYO
+        ァ: XA, ィ: XI, ゥ: XU, ェ: XE, ォ: XO,
+        ャ: XYA, ュ: XYU, ョ: XYO, ッ: XTSU,
+        ー: DASH
       }.freeze
     end
   end
